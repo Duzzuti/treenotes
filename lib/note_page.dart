697c6eeead22b735d3d31ebb5e715a4e1b07844c
Duzzuti <duzzuti@gmail.com>
2024-02-18 19:27:34 +0100

    Refactor loading screen

--- lib/note_page.dart
+++ lib/note_page.dart
@@ -3,6 +3,7 @@ import 'package:treenotes/dialogs/create_dialog.dart';
 import 'package:treenotes/database/helper.dart';
 import 'package:treenotes/dialogs/info_dialog.dart';
 import 'package:treenotes/widgets/custom_appbar.dart';
+import 'package:treenotes/widgets/loading_scaffold.dart';
 
 class NotePage extends StatefulWidget {
   final int nodeId;
@@ -51,224 +52,197 @@ class _NotePageState extends State<NotePage> {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        return Stack(
-          children: [
-            Scaffold(
-              appBar: CustomAppBar(
-                context: context,
-                title: isLoading ? "Loading..." : node!["title"],
-                actions: ActionDataList(
-                  actions: [
-                    ActionData(
-                      icon: Icons.add,
-                      onPressed: () {
-                        showDialog(
-                                context: context,
-                                builder: (context) => isLoading
-                                    ? const Dialog()
-                                    : CreateDialog(parentId: widget.nodeId),
-                                barrierDismissible: false)
-                            .then((value) => loadData());
-                      },
-                    ),
-                    ActionData(
-                      icon: Icons.home,
-                      onPressed: () {
-                        Navigator.popUntil(
-                          context,
-                          ModalRoute.withName('/'),
-                        );
-                      },
-                    ),
-                  ],
+        return LoadingScaffold(
+          appBar: CustomAppBar(
+            context: context,
+            title: isLoading ? "Loading..." : node!["title"],
+            actions: ActionDataList(
+              actions: [
+                ActionData(
+                  icon: Icons.add,
+                  onPressed: () {
+                    showDialog(
+                            context: context,
+                            builder: (context) => isLoading
+                                ? const Dialog()
+                                : CreateDialog(parentId: widget.nodeId),
+                            barrierDismissible: false)
+                        .then((value) => loadData());
+                  },
                 ),
-              ),
-              body: Column(
+                ActionData(
+                  icon: Icons.home,
+                  onPressed: () {
+                    Navigator.popUntil(
+                      context,
+                      ModalRoute.withName('/'),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+          body: Column(
+            children: [
+              Row(
                 children: [
-                  Row(
-                    children: [
-                      const SizedBox(width: 12),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.13,
-                        child: Text(
-                          'Note',
-                          style: TextStyle(
-                            color: Theme.of(context).colorScheme.secondary,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.65,
-                        child: Center(
-                          child: Text(
-                            'direct/indirect children',
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.secondary,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 8),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.08,
-                        child: Center(
-                          child: Text(
-                            'Go',
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.secondary,
-                              fontSize: 20,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  const SizedBox(width: 12),
                   SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.7,
-                    child: ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      physics: const AlwaysScrollableScrollPhysics(),
-                      itemBuilder: (context, index) {
-                        return Container(
-                          padding: const EdgeInsets.all(8),
-                          margin: const EdgeInsets.all(4),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.tertiary,
-                            border: Border.all(
-                              color: Theme.of(context).colorScheme.primary,
-                              width: 1,
-                            ),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Row(
-                            children: [
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.55,
-                                child: TextButton(
-                                  onPressed: () {
-                                    showDialog(
-                                            context: context,
-                                            builder: (context) => InfoDialog(
-                                                node: children![index]))
-                                        .then((value) => loadData());
-                                  },
-                                  child: Text(
-                                    children![index]["title"],
-                                    style: TextStyle(
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(width: 8),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.1,
-                                child: Center(
-                                  child: Text(
-                                    children![index]["num_children"] > 999
-                                        ? '>999'
-                                        : children![index]["num_children"]
-                                            .toString(),
-                                    maxLines: 1,
-                                    softWrap: false,
-                                    style: TextStyle(
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(width: 8),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.1,
-                                child: Center(
-                                  child: Text(
-                                    children![index]["num_descendants"] > 999
-                                        ? '>999'
-                                        : children![index]["num_descendants"]
-                                            .toString(),
-                                    maxLines: 1,
-                                    softWrap: false,
-                                    style: TextStyle(
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(width: 8),
-                              Center(
-                                child: InkWell(
-                                  hoverColor: Colors.red,
-                                  focusColor: Colors.red,
-                                  overlayColor:
-                                      MaterialStateProperty.all(Colors.red),
-                                  highlightColor: Colors.red,
-                                  splashColor: Colors.red,
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => NotePage(
-                                                nodeId: children![index]
-                                                    ["node_id"],
-                                              )),
-                                    ).then((value) => loadData());
-                                  },
-                                  child: SizedBox(
-                                    width:
-                                        MediaQuery.of(context).size.width * 0.1,
-                                    child: Icon(
-                                      Icons.arrow_forward_ios,
-                                      color:
-                                          Theme.of(context).colorScheme.primary,
-                                      size: 24,
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                      itemCount: children == null ? 0 : children!.length,
+                    width: MediaQuery.of(context).size.width * 0.13,
+                    child: Text(
+                      'Note',
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.secondary,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.65,
+                    child: Center(
+                      child: Text(
+                        'direct/indirect children',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.08,
+                    child: Center(
+                      child: Text(
+                        'Go',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.secondary,
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
-            ),
-            if (isLoading)
-              Scaffold(
-                // Darken the background
-                backgroundColor: Colors.black.withOpacity(0.7),
-                body: const Column(
-                  children: [
-                    Spacer(flex: 4),
-                    Expanded(
-                      child: Center(
-                        child: AspectRatio(
-                          aspectRatio: 1,
-                          child: SizedBox.expand(
-                            child: CircularProgressIndicator(
-                              color: Colors.white,
-                              strokeWidth: 6,
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.7,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: const EdgeInsets.all(8),
+                      margin: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.tertiary,
+                        border: Border.all(
+                          color: Theme.of(context).colorScheme.primary,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.55,
+                            child: TextButton(
+                              onPressed: () {
+                                showDialog(
+                                        context: context,
+                                        builder: (context) => InfoDialog(
+                                            node: children![index]))
+                                    .then((value) => loadData());
+                              },
+                              child: Text(
+                                children![index]["title"],
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(context).colorScheme.primary,
+                                  fontSize: 20,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
+                          const SizedBox(width: 8),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.1,
+                            child: Center(
+                              child: Text(
+                                children![index]["num_children"] > 999
+                                    ? '>999'
+                                    : children![index]["num_children"]
+                                        .toString(),
+                                maxLines: 1,
+                                softWrap: false,
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(context).colorScheme.primary,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.1,
+                            child: Center(
+                              child: Text(
+                                children![index]["num_descendants"] > 999
+                                    ? '>999'
+                                    : children![index]["num_descendants"]
+                                        .toString(),
+                                maxLines: 1,
+                                softWrap: false,
+                                style: TextStyle(
+                                  color:
+                                      Theme.of(context).colorScheme.primary,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 8),
+                          Center(
+                            child: InkWell(
+                              hoverColor: Colors.red,
+                              focusColor: Colors.red,
+                              overlayColor:
+                                  MaterialStateProperty.all(Colors.red),
+                              highlightColor: Colors.red,
+                              splashColor: Colors.red,
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => NotePage(
+                                            nodeId: children![index]
+                                                ["node_id"],
+                                          )),
+                                ).then((value) => loadData());
+                              },
+                              child: SizedBox(
+                                width:
+                                    MediaQuery.of(context).size.width * 0.1,
+                                child: Icon(
+                                  Icons.arrow_forward_ios,
+                                  color:
+                                      Theme.of(context).colorScheme.primary,
+                                  size: 24,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
                       ),
-                    ),
-                    Spacer(flex: 4),
-                  ],
+                    );
+                  },
+                  itemCount: children == null ? 0 : children!.length,
                 ),
               ),
-          ],
+            ],
+          ),
+          isLoading: isLoading,
         );
       },
     );
